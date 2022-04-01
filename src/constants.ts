8dfda8f274a2f2600466e3c0d1627f9c4a1673d5
gk <shinhwagk@outlook.com>
2022-04-01 19:42:53 +0800

    check

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
+import * as os from 'os';
+import * as path from 'path';
+
 export const schema = "tanka";
-export const temp = "/tmp/vscode-tanka";
+export const temp = path.join(os.tmpdir(), 'vscode-tanka');
